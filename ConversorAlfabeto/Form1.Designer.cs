75cb2b1e590a3b6469c4bdf6f096274372660389
eedhernandes <edsonfernandohernandes@gmail.com>
2021-05-23 15:33:36 -0300

    first commit

---
+++ ConversorAlfabeto/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ConversorAlfabeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(116, 98);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(100, 28);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(116, 56);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(295, 22);
+            this.txtResultado.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Resultado:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtTexto.Location = new System.Drawing.Point(116, 20);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(295, 22);
+            this.txtTexto.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(14, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Insira um texto:";
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(311, 98);
+            this.btnConverter.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(100, 28);
+            this.btnConverter.TabIndex = 3;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 147);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConverter);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor alfabeto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConverter;
+    }
+}
+
---
+++ ConversorAlfabeto/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorAlfabeto
+{
+    public partial class Form1 : Form
+    {
+        string regexLetra = "^[a-z]";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtTexto.Clear();
+            txtResultado.Clear();
+
+            txtTexto.Focus();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtTexto.Clear();
+            txtResultado.Clear();
+
+            txtTexto.Focus();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            if (txtTexto.Text.Trim().Length == 0)
+            {
+                DialogResult mensagem;
+                mensagem = MessageBox.Show("Preencha o campo em destaque e tente novamente!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                if (mensagem == DialogResult.OK)
+                {
+                    txtTexto.Focus();
+                    return;
+                }
+            }
+            else
+            {
+                string txt = txtTexto.Text;
+                string textoLetras = "";
+                string textoConvertido = "";
+
+                var textoSemFormatacao = txt.Split(' ');
+
+                for (int i = 0; i < textoSemFormatacao.Length; i++)
+                {
+                    if (textoLetras != "")
+                    {
+                        if (Regex.IsMatch(textoSemFormatacao[i], regexLetra))
+                        {
+                            textoLetras = textoLetras + " " + new String(textoSemFormatacao[i].Where(c => Char.IsLetter(c)).ToArray());
+                        }
+                    }
+                    else
+                    {
+                        if (Regex.IsMatch(textoSemFormatacao[i], regexLetra))
+                        {
+                            textoLetras = new String(textoSemFormatacao[i].Where(c => Char.IsLetter(c)).ToArray());
+                        }
+                    }
+                }
+
+                textoConvertido = textoLetras;
+
+                textoConvertido = textoConvertido.Replace("c", "3");
+                textoConvertido = textoConvertido.Replace("f", "6");
+                textoConvertido = textoConvertido.Replace("i", "9");
+                textoConvertido = textoConvertido.Replace("l", "12");
+                textoConvertido = textoConvertido.Replace("o", "15");
+                textoConvertido = textoConvertido.Replace("r", "18");
+                textoConvertido = textoConvertido.Replace("u", "21");
+                textoConvertido = textoConvertido.Replace("x", "24");
+
+                txtResultado.Text = textoConvertido;
+
+                txtTexto.Focus();
+            }
+        }
+    }
+}
